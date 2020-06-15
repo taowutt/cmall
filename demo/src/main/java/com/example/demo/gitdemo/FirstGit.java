d8bc28282482ee607379ad9a3b0c6fd6fbecb4bc
iccct <1498480894@qq.com>
2020-06-15 17:35:00 +0800

    提交一个任务

---
+++ demo/src/main/java/com/example/demo/gitdemo/FirstGit.java
@@ -0,0 +1,12 @@
+package com.example.demo.gitdemo;
+
+/**
+ * @author iccct
+ * @date 2020/06/15
+ * @time 17:29
+ **/
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("git demo");
+    }
+}
